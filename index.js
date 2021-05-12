359670b6747a30a35fe6cea1966041d82b685a1d
Benjamin Cheng <bencheng200@gmail.com>
2021-05-12 16:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let animal = ""
 
 function myAnimal() {
+  const animal = "dog"
   return animal
 }
 
 function yourAnimal() {
+  const animal = "cat"
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
